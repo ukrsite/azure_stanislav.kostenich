c0ef076656ad9a541ede8f7f0bfc5c31991d0506
Stanislav Kostenich <52426715+ukrsite@users.noreply.github.com>
2025-02-17 11:09:01 +0000

    Added Task 10

--- 08 Infrastructure as Code (IaC)  Monitoring And Logging/10 Terraform: Deploy a Production-Ready AKS Cluster with GitOps & Secret Management & Monitoring/terraform-project/main.tf
+++ 08 Infrastructure as Code (IaC)  Monitoring And Logging/10 Terraform: Deploy a Production-Ready AKS Cluster with GitOps & Secret Management & Monitoring/terraform-project/main.tf
@@ -17,7 +17,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = var.dns_prefix
-
+  azure_policy_enabled = true
+  
   default_node_pool {
     name       = "system"
     node_count = var.node_count
